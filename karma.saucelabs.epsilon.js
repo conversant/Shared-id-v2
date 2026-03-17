14424f3c46024c5d58f0c3f4f7a8e84f39166a25
Paul Yang <pyang@conversantmedia.com>
2026-03-17 13:17:31 -0700

    MPUB-21099 Use external Sauce Connect

---
+++ karma.saucelabs.epsilon.js
@@ -0,0 +1,45 @@
+const webpackConfig = require('./webpack.config')(undefined, {mode: 'development'});
+const browsers = require('./browsers.epsilon.json');
+const pkg = require('./package.json');
+
+const title = `${pkg.name} ${pkg.version} unit tests`;
+
+const tunnelName = process.env.SAUCE_TUNNEL_NAME 
+    || process.env.BAMBOO_BUILD_KEY 
+    || `${process.env.USER || 'local'}-sharedid-dev`;
+
+console.log(`Using Sauce Connect tunnel: ${tunnelName}`);
+
+module.exports = function(config) {
+    config.set({
+        frameworks: ['mocha'],
+        files: [
+            { pattern: 'test/**/*.spec.js' }
+        ],
+
+        preprocessors: {
+            'test/**/*.spec.js': ['webpack', 'sourcemap']
+        },
+
+        webpack: webpackConfig[0],
+
+        reporters: ['mocha', 'saucelabs', 'bamboo'],
+
+        sauceLabs: {
+            testName: title,
+            startConnect: false,
+            tunnelIdentifier: tunnelName,
+            verbose: true,
+            verboseDebugging: true
+        },
+
+        browsers: Object.keys(browsers),
+        customLaunchers: browsers,
+        browserDisconnectTimeout: 10000,
+        browserDisconnectTolerance: 1,
+        browserNoActivityTimeout:  4 * 60 * 1000,
+        captureTimeout: 4 * 60 * 1000,
+        concurrency: 2,
+        singleRun: true,
+    });
+};
